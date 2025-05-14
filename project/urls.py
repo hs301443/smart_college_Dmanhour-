d9a450b1386b288d20aeddf5a5bc58bd950022a5
hs301443 <hassanmoged590@gmail.com>
2025-05-15 00:38:29 +0300

    fix

--- project/urls.py
+++ project/urls.py
@@ -23,10 +23,11 @@ from rest_framework import permissions
 from project import settings
 
 
-
+def root_view(request):
+    return JsonResponse({"message": "Welcome to Smart College API 🚀"})
 
 urlpatterns = [
-
+       path('', root_view),  # هنا هنضيف الـ root view
     path('admin/', admin.site.urls),
     path('core/', include('apps.core.urls')),
     path('units/', include('apps.units.urls')),
