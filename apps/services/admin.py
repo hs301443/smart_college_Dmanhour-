7320c8c745d0da11aceb477ec20dbe66148a7055
hs301443 <hassanmoged590@gmail.com>
2025-05-15 23:10:39 +0300

    fixx

--- apps/services/admin.py
+++ apps/services/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import section,services
+from .models import section,acadmic_year
 
 admin.site.register(section)
-admin.site.register(services)
+admin.site.register(acadmic_year)
