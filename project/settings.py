554a4004e3378197d68d349e819cf159578e0d8e
hs301443 <hassanmoged590@gmail.com>
2025-05-20 13:39:48 +0300

    fix

--- project/settings.py
+++ project/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default= False, cast=bool)
 
-ALLOWED_HOSTS = ['smartcollegedmanhour-production.up.railway.app','localhost' ]
-CSRF_TRUSTED_ORIGINS = ['https://smartcollegedmanhour-production.up.railway.app/',]
+ALLOWED_HOSTS = ALLOWED_HOSTS = ['smartcollegedmanhour-production.up.railway.app', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://smartcollegedmanhour-production.up.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
