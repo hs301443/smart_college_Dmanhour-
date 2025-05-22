520e8702ad8524e00e65264e950015c870016062
hs301443 <hassanmoged590@gmail.com>
2025-05-22 13:10:18 +0300

    fix

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default= True, cast=bool)
+DEBUG = config('DEBUG', default= False, cast=bool)
 
 ALLOWED_HOSTS = ALLOWED_HOSTS = ['smartcollegedmanhour-production.up.railway.app', 'localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://smartcollegedmanhour-production.up.railway.app']
