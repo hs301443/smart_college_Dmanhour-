ff8e39bd05bb2565b60dbcd272d70d78b49bb831
hs301443 <hassanmoged590@gmail.com>
2025-06-21 01:57:53 +0300

    fix

--- apps/studentportal/urls.py
+++ apps/studentportal/urls.py
@@ -1,11 +1,12 @@
 # studentportal/urls.py
 from django.urls import path
-from .views import AllNotificationsView, CreateNotificationView, DeleteNotificationView, StudentPortalAPIView, UpdateNotificationView, UserNotificationsView
+from .views import AllNotificationsView, CreateNotificationView, DeleteNotificationView, StudentPortalAPIView, StudentPortalImageAPIView, UpdateNotificationView, UserNotificationsView
 
 urlpatterns = [
     path('portal/', StudentPortalAPIView.as_view()),              # list + create
     path('portal/<int:pk>/', StudentPortalAPIView.as_view()),     # detail, update, delete
-    
+    path('portal/image/', StudentPortalImageAPIView.as_view()),           # GET all - POST
+    path('portal/image/<int:pk>/', StudentPortalImageAPIView.as_view()),
     
     
      path('notifications/', CreateNotificationView.as_view(), name='create-notification'),
